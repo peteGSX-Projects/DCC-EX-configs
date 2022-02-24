00753c58af39876564793c0694419f889174a546
Peter Cole <>
2022-02-24 22:12:29 +1000

    Optimised decoupler macro

--- myRoutes.h
+++ myRoutes.h
@@ -95,15 +95,15 @@ ROUTE(5, "Stall 1 to industry loop")
 // Decoupler ROUTES
 #define DECOUPLE_TIME 10000
 
-#define DECOUPLER(r, p1, desc) \
-    ROUTE(r, desc) \
+#define DECOUPLER(p1, desc) \
+    ROUTE(300+p1, desc) \
         SERVO(p1, 10, Fast) \
         DELAY(DECOUPLE_TIME) \
         SERVO(p1, 200, Fast) \
     DONE
 
-DECOUPLER(401, 101, "Yard declouper 1 - right branch")
-DECOUPLER(402, 102, "Yard declouper 2 - run around entry")
-DECOUPLER(403, 103, "Run around declouper")
-DECOUPLER(404, 104, "Stall 7 declouper")
+DECOUPLER(101, "Yard declouper 1 - right branch")
+DECOUPLER(102, "Yard declouper 2 - run around entry")
+DECOUPLER(103, "Run around declouper")
+DECOUPLER(104, "Stall 7 declouper")
 DONE

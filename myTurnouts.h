ab005c0a3b87845f8bc51732c300615fd54832be
Peter Cole <>
2022-03-21 17:12:54 +1000

    Created pair macro

--- myTurnouts.h
+++ myTurnouts.h
@@ -48,12 +48,6 @@ DONE
 
 // Turnout definitions using the ROKUHANTURNOUT macro
 
-// Main to industry loop turnout definitions
-ROKUHANTURNOUT(101, 164, 172, "Main to industry loop 1", MA_IND_1) // Pair of left turnouts
-ROKUHANTURNOUT(102, 165, 173, "Industry loop to main 1", IND_MA_1)
-ROKUHANTURNOUT(103, 166, 174, "Main to industry loop 2", MA_IND_2) // Pair of right turnouts)
-ROKUHANTURNOUT(104, 167, 175, "Industry loop to main 2", IND_MA_2)
-
 // Industry loop turnouts
 ROKUHANTURNOUT(105, 168, 176, "Yard entrance", YD_E)
 ROKUHANTURNOUT(106, 169, 177, "Inner industry loop entrance 1", IIND_E1) // Behind roundhouse
@@ -70,32 +64,74 @@ ROKUHANTURNOUT(114, 185, 193, "Yard 1", YD_1) // First left turnout after entran
 ROKUHANTURNOUT(115, 186, 194, "Runaround siding entry", RUN_E) // Entry to siding with runaround
 ROKUHANTURNOUT(116, 187, 195, "Rolling road entry", RR_E)
 ROKUHANTURNOUT(117, 196, 204, "Turntable entry", TT_E)
-ROKUHANTURNOUT(118, 197, 205, "Runaround 1", RUN_1) // Pair of right turnouts for runaround
-ROKUHANTURNOUT(119, 198, 206, "Runaround 2", RUN_2)
 ROKUHANTURNOUT(120, 199, 207, "Yard 2", YD_2) // Right turnout
 ROKUHANTURNOUT(121, 200, 208, "Yard 3", YD_3) // Left turnout
 ROKUHANTURNOUT(122, 201, 209, "Yard 4", YD_4) // Left turnout
 DONE
 
+/* Rokuhan turnout pair macro
+*  This macro defines Rokuhan turnouts as pairs as well as ensuring that these
+*  turnouts are driven only by a short pulse to prevent coil burn out.
+*
+*  Two turnouts are configured by this macro to operate together when closing/throwing.
+*
+*  Call this macro with:
+*  ROKUHANTURNOUTPAIR(t1, t1p1, t1p2, t1desc, t1ali, t2, t2p1, t2p2, t2desc, t2ali)
+*
+*  Where:
+*  t1 = turnout ID for the first turnout
+*  t1p1 = direction pin for the first turnout
+*  t1p2 = enable pin for the first turnout
+*  t1desc = description of the first turnout
+*  t1ali = alias assigned to the first turnout ID
+*  t2 = turnout ID for the second turnout
+*  t2p1 = direction pin for the second turnout
+*  t2p2 = enable pin for the second turnout
+*  t2desc = description of the second turnout
+*  t2ali = alias assigned to the second turnout ID
+*
+*  Adjust the PULSE time (in ms) below as required, 25 seems reliable (defined above).
+*
+*  When a turnout command is sent to one of the defined turnouts, the macro
+*  will close or throw the turnout using the defined PULSE time.
+*
+*  This also ensures all turnouts are defined as part of the compilation.
+*/
+#define ROKUHANTURNOUTPAIR(t1, t1p1, t1p2, t1desc, t1ali, t2, t2p1, t2p2, t2desc, t2ali) \
+PIN_TURNOUT(t1, 0, t1desc) \
+ALIAS(t1ali, t1) \
+PIN_TURNOUT(t2, 0, t2desc) \
+ALIAS(t2ali, t2) \
+DONE \
+ONCLOSE(t1) \
+SET(t1p1)SET(t2p1) \
+SET(t1p2)SET(t2p2)DELAY(PULSE)RESET(t1p2)RESET(t2p2) \
+DONE \
+ONCLOSE(t2) \
+SET(t1p1)SET(t2p1) \
+SET(t1p2)SET(t2p2)DELAY(PULSE)RESET(t1p2)RESET(t2p2) \
+DONE \
+ONTHROW(t1) \
+RESET(t1p1)RESET(t2p1) \
+SET(t1p2)SET(t2p2)DELAY(PULSE)RESET(t1p2)RESET(t2p2) \
+DONE \
+ONTHROW(t2) \
+RESET(t1p1)RESET(t2p1) \
+SET(t1p2)SET(t2p2)DELAY(PULSE)RESET(t1p2)RESET(t2p2) \
+DONE
+
+// Turnout definitions using the ROKUHANTURNOUTPAIR macro
+
+// Main to industry loop turnout definitions
+ROKUHANTURNOUTPAIR(101, 164, 172, "Main to industry loop 1", MA_IND_1, 102, 165, 173, "Industry loop to main 1", IND_MA_1) // Pair of left turnouts
+ROKUHANTURNOUTPAIR(103, 166, 174, "Main to industry loop 2", MA_IND_2, 104, 167, 175, "Industry loop to main 2", IND_MA_2) // Pair of right turnouts)
+
+// Runaround yard turnouts
+ROKUHANTURNOUTPAIR(118, 197, 205, "Runaround 1", RUN_1, 119, 198, 206, "Runaround 2", RUN_2) // Pair of right turnouts for runaround
+
 /*
 *  Define the turnout pairs that need to operate simultaneously
 */
-
-// Ensure main to industry loops turnouts operate together
-ONCLOSE(MA_IND_1) CLOSE(IND_MA_1) DONE
-ONTHROW(MA_IND_1) THROW(IND_MA_1) DONE
-ONCLOSE(IND_MA_1) CLOSE(MA_IND_1) DONE
-ONTHROW(IND_MA_1) THROW(MA_IND_1) DONE
-ONCLOSE(MA_IND_2) CLOSE(IND_MA_2) DONE
-ONTHROW(MA_IND_2) THROW(IND_MA_2) DONE
-ONCLOSE(IND_MA_2) CLOSE(MA_IND_2) DONE
-ONTHROW(IND_MA_2) THROW(MA_IND_2) DONE
-
-// Ensure the runaround turnouts operate together
-ONCLOSE(RUN_1) CLOSE(RUN_2) DONE
-ONTHROW(RUN_1) THROW(RUN_2) DONE
-ONCLOSE(RUN_2) CLOSE(RUN_1) DONE
-ONTHROW(RUN_2) THROW(RUN_1) DONE
 
 // Turnout power relay
 ALIAS(TOUT_PWR, 211)

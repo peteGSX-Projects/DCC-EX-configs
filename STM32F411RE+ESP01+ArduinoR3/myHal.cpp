daee61511d4e9efa0bcf466de5088bf6794079c4
peteGSX <peteracole@outlook.com.au>
2022-08-15 06:37:17 +1000

    Clean up and add STM32

--- STM32F411RE+ESP01+ArduinoR3/myHal.cpp
+++ STM32F411RE+ESP01+ArduinoR3/myHal.cpp
@@ -2,8 +2,7 @@
 
 // Include devices you need.
 #include "IODevice.h"
-#include "IO_EXTurntable.h"
-#include "IO_RotaryEncoder.h"
+
 //==========================================================================
 // The function halSetup() is invoked from CS if it exists within the build.
 // The setup calls are included between the open and close braces "{ ... }".
@@ -11,18 +10,7 @@
 //==========================================================================
 
 void halSetup() {
-  MCP23017::create(196, 16, 0x22);
-
-// Five defined below ready for allocation
-  //MCP23017::create(212, 16, 0x23);
-  //MCP23017::create(228, 16, 0x24);
-  //MCP23017::create(244, 16, 0x25);
-  //MCP23017::create(260, 16, 0x26);
-  //MCP23017::create(276, 16, 0x27);
-
-  EXTurntable::create(600, 1, 0x60);
-
-  RotaryEncoder::create(700, 1, 0x80);
+  
 }
 
 #endif
---
+++ STM32F411RE+ESP01+ArduinoR3/myWiFiExample.h
@@ -0,0 +1,13 @@
+/*
+myWiFi.h
+
+Separating WiFi SSID and password out to allow config.h to be committed to
+a Git repo without storing secrets.
+
+Need to add an include at the top of config.h:
+
+#include "myWiFi.h"
+*/
+
+#define WIFI_SSID "<SSID>"
+#define WIFI_PASSWORD "<password>"

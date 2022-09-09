74986ccc13b71bbe206c5002c52ddc93e42d9518
peteGSX <peteracole@outlook.com.au>
2022-09-09 11:47:02 +1000

    Fix typo

--- Mega2560+WiFi+Keyestudio/myTurntable.h
+++ Mega2560+WiFi+Keyestudio/myTurntable.h
@@ -15,14 +15,14 @@
     FREE(reserve_id) \
     DONE
 
-EX_TURNTABLE(TTROUTE_1, EXTT_1, 600, 114, Turn, "Roundhose stall 1")
-EX_TURNTABLE(TTROUTE_2, EXTT_1, 600, 194, Turn, "Roundhose stall 2")
-EX_TURNTABLE(TTROUTE_3, EXTT_1, 600, 274, Turn, "Roundhose stall 3")
-EX_TURNTABLE(TTROUTE_4, EXTT_1, 600, 354, Turn, "Roundhose stall 4")
-EX_TURNTABLE(TTROUTE_5, EXTT_1, 600, 434, Turn, "Roundhose stall 5")
-EX_TURNTABLE(TTROUTE_6, EXTT_1, 600, 514, Turn, "Roundhose stall 6")
-EX_TURNTABLE(TTROUTE_7, EXTT_1, 600, 594, Turn, "Roundhose stall 7")
-EX_TURNTABLE(TTROUTE_8, EXTT_1, 600, 674, Turn, "Roundhose stall 8")
+EX_TURNTABLE(TTROUTE_1, EXTT_1, 600, 114, Turn, "Roundhouse stall 1")
+EX_TURNTABLE(TTROUTE_2, EXTT_1, 600, 194, Turn, "Roundhouse stall 2")
+EX_TURNTABLE(TTROUTE_3, EXTT_1, 600, 274, Turn, "Roundhouse stall 3")
+EX_TURNTABLE(TTROUTE_4, EXTT_1, 600, 354, Turn, "Roundhouse stall 4")
+EX_TURNTABLE(TTROUTE_5, EXTT_1, 600, 434, Turn, "Roundhouse stall 5")
+EX_TURNTABLE(TTROUTE_6, EXTT_1, 600, 514, Turn, "Roundhouse stall 6")
+EX_TURNTABLE(TTROUTE_7, EXTT_1, 600, 594, Turn, "Roundhouse stall 7")
+EX_TURNTABLE(TTROUTE_8, EXTT_1, 600, 674, Turn, "Roundhouse stall 8")
 EX_TURNTABLE(TTROUTE_9, EXTT_1, 600, 114, Turn, "Storage track")
 EX_TURNTABLE(TTROUTE_10, EXTT_1, 600, 2500, Turn, "Layout connection")
 EX_TURNTABLE(TTROUTE_11, EXTT_1, 600, 2660, Turn, "Home turntable")

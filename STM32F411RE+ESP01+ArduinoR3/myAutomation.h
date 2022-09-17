d79c501a93e3b817d9a98932ae9ea20e7257ec84
peteGSX <peteracole@outlook.com.au>
2022-09-18 09:45:19 +1000

    More STM32

--- STM32F411RE+ESP01+ArduinoR3/myAutomation.h
+++ STM32F411RE+ESP01+ArduinoR3/myAutomation.h
@@ -1,7 +1,9 @@
 CLOSE(101)
+CLOSE(102)
 
 DONE
 
+// Turnouts
 #define PULSE 50
 
 #define ROKUHANTURNOUT(t, p1, p2, desc, ali) \
@@ -19,3 +21,51 @@ DONE \
 
 ROKUHANTURNOUT(101, 50, 51, "Turnout 1", TRN_1)
 ROKUHANTURNOUT(102, 2, 4, "Turnout 2", TRN_2)
+
+// EX-Turntable routes
+#define EX_TURNTABLE(route_id, reserve_id, vpin, steps, activity, desc) \
+  ROUTE(route_id, desc) \
+    RESERVE(reserve_id) \
+    MOVETT(vpin, steps, activity) \
+    WAITFOR(vpin) \
+    FREE(reserve_id) \
+    DONE
+
+// Turntable
+EX_TURNTABLE(TTROUTE1, TURNTABLE, 600, 500, Turn, "Position 1")
+
+// Traverser
+EX_TURNTABLE(TRROUTE1, TRAVERSER, 601, 500, Turn, "Position 1")
+
+// Rotary encoder routes
+ONCHANGE(700)
+  IFRE(700, 5)
+    START(TTROUTE1)
+  ENDIF
+  DONE
+
+
+// Loco roster entries
+ROSTER(2004, "QR 2004", "Lights")
+ROSTER(2006, "QR 2006", "Lights")
+ROSTER(2010, "QR 2010", "Lights")
+
+// Aliases
+ALIAS(TURNTABLE, 255)
+ALIAS(TRAVERSER, 254)
+ALIAS(TTROUTE1)
+ALIAS(TTROUTE2)
+ALIAS(TTROUTE3)
+ALIAS(TTROUTE4)
+ALIAS(TTROUTE5)
+ALIAS(TTROUTE6)
+ALIAS(TTROUTE7)
+ALIAS(TTROUTE8)
+ALIAS(TRROUTE1)
+ALIAS(TRROUTE2)
+ALIAS(TRROUTE3)
+ALIAS(TRROUTE4)
+ALIAS(TRROUTE5)
+ALIAS(TRROUTE6)
+ALIAS(TRROUTE7)
+ALIAS(TRROUTE8)

d50b2a4a351af5b9ad26833063199d4c250d290e
peteGSX <peteracole@outlook.com.au>
2022-09-18 05:27:26 +1000

    Update STM32 turnouts

---
+++ STM32F411RE+ESP01+ArduinoR3/myAutomation.h
@@ -0,0 +1,30 @@
+CLOSE(101)
+
+DONE
+
+#define PULSE 50
+
+#define ROKUHANTURNOUTPAIR(t1, t1p1, t1p2, t1desc, t1ali, t2, t2p1, t2p2, t2desc, t2ali) \
+PIN_TURNOUT(t1, 0, t1desc) \
+ALIAS(t1ali, t1) \
+PIN_TURNOUT(t2, 0, t2desc) \
+ALIAS(t2ali, t2) \
+DONE \
+ONCLOSE(t1) \
+SET(t1p1)SET(t2p1) \
+SET(t1p2)SET(t2p2)DELAY(PULSE)RESET(t1p2)RESET(t2p2) \
+DONE \
+ONCLOSE(t2) \
+SET(t1p1)SET(t2p1) \
+SET(t1p2)SET(t2p2)DELAY(PULSE)RESET(t1p2)RESET(t2p2) \
+DONE \
+ONTHROW(t1) \
+RESET(t1p1)RESET(t2p1) \
+SET(t1p2)SET(t2p2)DELAY(PULSE)RESET(t1p2)RESET(t2p2) \
+DONE \
+ONTHROW(t2) \
+RESET(t1p1)RESET(t2p1) \
+SET(t1p2)SET(t2p2)DELAY(PULSE)RESET(t1p2)RESET(t2p2) \
+DONE
+
+ROKUHANTURNOUTPAIR(101, 50, 51, "Turnout 1", TRN_1, 102, 2, 4, "Turnout 2", TRN_2) // Pair of turnouts

7d9368880d6191f1413412ec05c5db0d7018915e
peteGSX <peteracole@outlook.com.au>
2023-04-15 05:22:47 +1000

    Start on pin aliases

---
+++ Garage-Layout/EX-CommandStation/myPinAliases.h
@@ -0,0 +1,7 @@
+/* myPinAliases.h
+* This file contains aliases for the pin names, primarily for the point controller to
+* save having to remember which pin is for direction vs. enable for each point.
+*/
+
+ALIAS(POINT1_DIR,186)
+ALIAS(POINT1_EN,176)

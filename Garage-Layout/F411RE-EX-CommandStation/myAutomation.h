a79579689f5670a543bb5c9c7e811503a44ba1ca
peteGSX <97784652+peteGSX@users.noreply.github.com>
2024-01-10 11:36:38 +1000

    Layout updates

--- Garage-Layout/F411RE-EX-CommandStation/myAutomation.h
+++ Garage-Layout/F411RE-EX-CommandStation/myAutomation.h
@@ -1,11 +1,11 @@
 // Define I2C devices here
 #include "IO_RotaryEncoder.h"
-#include "myPinAliases.h"
+#include "myAliases.h"
 HAL(MCP23017, 164, 16, 0x20)
 HAL(MCP23017, 180, 16, 0x21)
 HAL(MCP23017, 196, 16, 0x22)
-HAL(EXTurntable, 600, 1, 0x60)
-HAL(RotaryEncoder, 700, 3, 0x70)
+HAL(EXTurntable, EXTT_VPIN, 1, 0x60)
+HAL(RotaryEncoder, RE_VPIN, 3, 0x70)
 HAL(EXIOExpander, 800, 97, 0x66)
 
 // These things need to happen at startup
--- Garage-Layout/F411RE-EX-CommandStation/myTurntable.h
+++ Garage-Layout/F411RE-EX-CommandStation/myTurntable.h
@@ -1,4 +1,4 @@
-/* myTurtable.h
+/* myTurntable.h
 *  This file contains all turntable macros and definitions.
 *
 *  Any related activities that need to occur on startup need to
@@ -6,37 +6,36 @@
 *  correct order when compiled.
 */
 
+// Create EX-Turntable objects
+EXTT_TURNTABLE(EXTT,600,180,"QR Turntable")
+TT_ADDPOSITION(EXTT,1,676,166,"Stall 1")
+TT_ADDPOSITION(EXTT,2,738,173,"Stall 2")
+TT_ADDPOSITION(EXTT,3,800,180,"Stall 3")
+TT_ADDPOSITION(EXTT,4,862,187,"Stall 4")
+TT_ADDPOSITION(EXTT,5,924,194,"Stall 5")
+TT_ADDPOSITION(EXTT,6,987,201,"Stall 6")
+TT_ADDPOSITION(EXTT,7,2276,76,"Storage")
+TT_ADDPOSITION(EXTT,8,2400,8,"Yard")
+
 // Define the turntable route macro
-#define EX_TURNTABLE(route_id, reserve_id, vpin, steps, activity, desc, rotary_encoder, position) \
+#define EX_TURNTABLE(route_id,tt_id,position,activity,desc,rotary_encoder) \
   ROUTE(route_id, desc) \
-    RESERVE(reserve_id) \
+    RESERVE(tt_id) \
     SET(1+rotary_encoder) \
     SERVO(2+rotary_encoder, position, Instant) \
-    MOVETT(vpin, steps, activity) \
-    WAITFOR(vpin) \
+    ROTATE(tt_id,position,activity) \
+    WAITFORTT(tt_id) \
     RESET(1+rotary_encoder) \
-    FREE(reserve_id) \
+    FREE(tt_id) \
     DONE
 
-EX_TURNTABLE(TTROUTE_1, EXTT_1, 600, 676, Turn, "Roundhouse stall 1", 700, 1)
-EX_TURNTABLE(TTROUTE_2, EXTT_1, 600, 738, Turn, "Roundhouse stall 2", 700, 2)
-EX_TURNTABLE(TTROUTE_3, EXTT_1, 600, 800, Turn, "Roundhouse stall 3", 700, 3)
-EX_TURNTABLE(TTROUTE_4, EXTT_1, 600, 862, Turn, "Roundhouse stall 4", 700, 4)
-EX_TURNTABLE(TTROUTE_5, EXTT_1, 600, 924, Turn, "Roundhouse stall 5", 700, 5)
-EX_TURNTABLE(TTROUTE_6, EXTT_1, 600, 987, Turn, "Roundhouse stall 6", 700, 6)
-EX_TURNTABLE(TTROUTE_7, EXTT_1, 600, 2276, Turn, "Storage track", 700, 7)
-EX_TURNTABLE(TTROUTE_8, EXTT_1, 600, 2400, Turn, "Yard connection", 700, 8)
-EX_TURNTABLE(TTROUTE_9, EXTT_1, 600, 0, Home, "Home turntable", 700, 0)
-EX_TURNTABLE(TTROUTE_10, EXTT_1, 600, 0, Calibrate, "Calibrate turntable", 700, 9)
-
-ALIAS(EXTT_1, 255)
-ALIAS(TTROUTE_1)
-ALIAS(TTROUTE_2)
-ALIAS(TTROUTE_3)
-ALIAS(TTROUTE_4)
-ALIAS(TTROUTE_5)
-ALIAS(TTROUTE_6)
-ALIAS(TTROUTE_7)
-ALIAS(TTROUTE_8)
-ALIAS(TTROUTE_9)
-ALIAS(TTROUTE_10)
+EX_TURNTABLE(TTROUTE_1, EXTT, 1, Turn, "Roundhouse stall 1", RE_VPIN)
+EX_TURNTABLE(TTROUTE_2, EXTT, 2, Turn, "Roundhouse stall 2", RE_VPIN)
+EX_TURNTABLE(TTROUTE_3, EXTT, 3, Turn, "Roundhouse stall 3", RE_VPIN)
+EX_TURNTABLE(TTROUTE_4, EXTT, 4, Turn, "Roundhouse stall 4", RE_VPIN)
+EX_TURNTABLE(TTROUTE_5, EXTT, 5, Turn, "Roundhouse stall 5", RE_VPIN)
+EX_TURNTABLE(TTROUTE_6, EXTT, 5, Turn, "Roundhouse stall 6", RE_VPIN)
+EX_TURNTABLE(TTROUTE_7, EXTT, 7, Turn, "Storage track", RE_VPIN)
+EX_TURNTABLE(TTROUTE_8, EXTT, 8, Turn, "Yard connection", RE_VPIN)
+EX_TURNTABLE(TTROUTE_9, EXTT, 0, Home, "Home turntable", RE_VPIN)
+EX_TURNTABLE(TTROUTE_10, EXTT, 0, Calibrate, "Calibrate turntable", RE_VPIN)
